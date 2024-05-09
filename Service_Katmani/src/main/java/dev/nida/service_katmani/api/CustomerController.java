b740824551730d781a3079a3d3ee027457911814
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-05-09 15:53:10 +0300

    added IoC and DI methods

--- Service_Katmani/src/main/java/dev/nida/service_katmani/api/CustomerController.java
+++ Service_Katmani/src/main/java/dev/nida/service_katmani/api/CustomerController.java
@@ -2,6 +2,7 @@ package dev.nida.service_katmani.api;
 
 import dev.nida.service_katmani.Customer;
 import dev.nida.service_katmani.business.abstracts.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    /*
+
+    // IOC : Inversion of Control
+    // 3 farklı yöntem var: Constructor, Field, Setter
+
+    // CONTRUCTOR metoduyla
+    private final ICustomerService customerService;
+    @Autowired // 4.3'den sonra spring bunu otomatik algılıyor @Autowired notasyonu kaldırılabilir.
+    public CustomerController(ICustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    // SETTER metoduyla
+    private ICustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(ICustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    // FIELD metoduyla
+    @Autowired
+    private ICustomerService customerService;
+
+     */
+
+
+    // MANUEL yöntem
+    /*
     private final ICustomerService customerService;
 
     public CustomerController(ICustomerService customerService) {
@@ -26,5 +56,6 @@ public class CustomerController {
     public Customer getById(@PathVariable("id") int id) {
         return customerService.getById(id);
     }
+     */
 
 }
